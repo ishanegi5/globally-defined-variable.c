8d471e5f7bd8dd7aa2905c2a3d1965bc1bb1e104
Isha Negi <itsishanegi@gmail.com>
2024-09-22 08:03:42 +0530

    Add files via upload

---
+++ globally-defined-variable.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int n=17;
+
+int fun()
+
+{ n++;
+
+return n;
+
+}
+
+int fun1()
+
+{ n=7;
+ 
+n++;
+ 
+return n;
+
+}
+
+int main()
+
+{ printf("%d ",fun());
+  
+printf("%d ",fun());
+  
+printf("%d \n",fun());
+  
+printf("%d ",fun1());
+  
+printf("%d ",fun1());
+  
+printf("%d ",fun1());
+
+return 0;
+
+}
